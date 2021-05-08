c1618f3e041e6970f32a8804ae1cc9f3da1847ac
koehlersimon <info@simon-koehler.com>
2021-05-08 09:35:28 -0500

    New fields and relation table

--- ext_tables.sql
+++ ext_tables.sql
@@ -4,6 +4,7 @@ CREATE TABLE tx_showcase_domain_model_project (
 	pid int(11) DEFAULT '0' NOT NULL,
 
 	title varchar(255) DEFAULT '' NOT NULL,
+	slug varchar(2048),
 	description text DEFAULT '' NOT NULL,
 	teaser text DEFAULT '' NOT NULL,
 	media int(11) unsigned DEFAULT '0' NOT NULL,
@@ -12,6 +13,7 @@ CREATE TABLE tx_showcase_domain_model_project (
 	show_layout varchar(50) DEFAULT '' NOT NULL,
 	preview_image int(11) unsigned DEFAULT '0' NOT NULL,
 	background_class varchar(50) DEFAULT '' NOT NULL,
+	related int(11) unsigned DEFAULT '0' NOT NULL,
 
 	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
 	crdate int(11) unsigned DEFAULT '0' NOT NULL,
@@ -64,4 +66,13 @@ CREATE TABLE tx_showcase_domain_model_url (
 	KEY parent (pid),
 	KEY language (l10n_parent,sys_language_uid)
 
+);
+
+CREATE TABLE tx_showcase_domain_model_project_related_mm (
+	uid_local int(11) DEFAULT '0' NOT NULL,
+	uid_foreign int(11) DEFAULT '0' NOT NULL,
+	sorting int(11) DEFAULT '0' NOT NULL,
+	sorting_foreign int(11) DEFAULT '0' NOT NULL,
+	KEY uid_local (uid_local),
+	KEY uid_foreign (uid_foreign)
 );
