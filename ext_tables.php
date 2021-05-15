20dff3bc100792a61a4d93f095086b1d21c5f19f
koehlersimon <info@simon-koehler.com>
2021-05-15 13:50:13 -0500

    Add JS, TS and Styles to the backend

--- ext_tables.php
+++ ext_tables.php
@@ -19,3 +19,19 @@
       'l10n_display' => 'hideDiff',
    )
 );
+
+// Add TSConfig to the Backend
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
+    mod.web_layout.tt_content.preview.list.showcase_showcase = EXT:showcase/Resources/Private/Templates/Preview/Showcase.html
+');
+
+// Add CSS to the Backend
+$GLOBALS['TBE_STYLES']['skins']['showcase'] = [];
+$GLOBALS['TBE_STYLES']['skins']['showcase']['name'] = 'showcase';
+$GLOBALS['TBE_STYLES']['skins']['showcase']['stylesheetDirectories'] = [
+    'theme' => 'EXT:showcase/Resources/Public/Css/Backend'
+];
+
+// Add JavaScript to the Backend
+$renderer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Page\PageRenderer::class);
+$renderer->addJsFile('EXT:showcase/Resources/Public/Js/ExtensionFeedback.js', 'text/javascript', false, false, '', true,  '|', false, '');
