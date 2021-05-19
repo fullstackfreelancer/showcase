d7dab279d6ca64576f476c3343389ee7192ab991
koehlersimon <info@simon-koehler.com>
2021-05-19 06:47:45 -0500

    Change plugin name

--- Resources/Public/Js/Classes/AjaxLoader.js
+++ Resources/Public/Js/Classes/AjaxLoader.js
@@ -2,7 +2,7 @@ class AjaxLoader {
 
     loadProject(projectUid,callback,targetObject){
         let request = new XMLHttpRequest();
-        request.open("GET", SHOWCASE.baseurl+'?type=78&tx_showcase_showcase[project]='+projectUid, true);
+        request.open("GET", SHOWCASE.baseurl+'?type=78&tx_showcase_show[project]='+projectUid, true);
         request.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
                 callback(this.responseText,targetObject);
