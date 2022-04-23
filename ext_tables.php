98aae82e7bafcdcf492c2472e09dcf4d27f24878
koehlersimon <info@simon-koehler.com>
2022-04-23 09:59:03 -0500

    Remove call of "makeCategorizable"

--- ext_tables.php
+++ ext_tables.php
@@ -2,24 +2,6 @@
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_showcase_domain_model_project');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_showcase_domain_model_url');
 
-// Add an extra categories selection field to the pages table
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::makeCategorizable(
-   'showcase',
-   'tx_showcase_domain_model_project',
-   // Do not use the default field name ("categories") for pages, tt_content, sys_file_metadata, which is already used
-   'categories',
-   array(
-      // Set a custom label
-      'label' => 'Categories',
-      // This field should not be an exclude-field
-      'exclude' => FALSE,
-      // string (keyword), see TCA reference for details
-      'l10n_mode' => 'exclude',
-      // list of keywords, see TCA reference for details
-      'l10n_display' => 'hideDiff',
-   )
-);
-
 // Add TSConfig to the Backend
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
     mod.web_layout.tt_content.preview.list.showcase_list = EXT:showcase/Resources/Private/Templates/Preview/Showcase.html
