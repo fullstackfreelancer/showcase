781d96586f26d5b5d99cd9f56c15f2618547c9c0
fullstackfreelancer <simonkoehler1978@gmail.com>
2023-12-11 07:40:27 -0500

    Remove old code

--- Resources/Public/Js/ExtensionFeedback.js
+++ Resources/Public/Js/ExtensionFeedback.js
@@ -1,32 +1,5 @@
 document.addEventListener('DOMContentLoaded',function(){
 
-    let extensionFeedbackButtons = document.querySelectorAll('.extension-feedback-modal-button');
-    let openExtensionFeedbackModal = function(){
-        require([
-           'jquery',
-           'TYPO3/CMS/Backend/Modal'
-           ], function ($, Modal) {
 
-              var configuration = {
-                 type: Modal.types.ajax,
-                 title: 'Extension Support',
-                 content: 'https://simonkoehler.com/contact',
-                 size: Modal.sizes.large,
-                 callback: function(currentModal) {
-                    currentModal.find('.t3js-modal-body').addClass('bg-dark');
-                 }
-              };
-              Modal.advanced(configuration);
-
-           });
-    }
-
-    if(extensionFeedbackButtons){
-        extensionFeedbackButtons.forEach((btn, i) => {
-            btn.addEventListener('click',function(e){
-                openExtensionFeedbackModal();
-            });
-        });
-    }
 
 });
