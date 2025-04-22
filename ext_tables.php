5d21f35e917406b23d605190d4ada4d5e63d7712
fullstackfreelancer <simonkoehler1978@gmail.com>
2025-04-22 07:51:32 -0500

    Remove obsolete code

--- ext_tables.php
+++ ext_tables.php
@@ -1,6 +1,2 @@
 <?php
 $GLOBALS['TYPO3_CONF_VARS']['BE']['stylesheets']['showcase'] = 'EXT:showcase/Resources/Public/Css/Backend';
-
-// Add JavaScript to the Backend
-$renderer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Page\PageRenderer::class);
-$renderer->addJsFile('EXT:showcase/Resources/Public/Js/ExtensionFeedback.js', 'text/javascript', false, false, '', true,  '|', false, '');
