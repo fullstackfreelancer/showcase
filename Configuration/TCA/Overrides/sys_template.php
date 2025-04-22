f101138f1f5683ee8e378e06ec1790344c5fa3c8
fullstackfreelancer <simonkoehler1978@gmail.com>
2025-04-22 07:44:54 -0500

    Re-organize files

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -5,15 +5,3 @@
     'Configuration/TypoScript',
     'Showcase: Basic Setup'
 );
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    'showcase',
-    'Configuration/TypoScript/Bootstrap4',
-    'Showcase: Bootstrap 4 Templates'
-);
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    'showcase',
-    'Configuration/TypoScript/Bootstrap5',
-    'Showcase: Bootstrap 5 Templates'
-);
