58257207535ba221f43cb5f9501ea86f8d70ab01
fullstackfreelancer <simonkoehler1978@gmail.com>
2025-05-05 18:14:21 -0500

    Add fields for translations

--- Configuration/TCA/tx_showcase_domain_model_project.php
+++ Configuration/TCA/tx_showcase_domain_model_project.php
@@ -9,7 +9,9 @@ return [
         'tstamp' => 'tstamp',
         'crdate' => 'crdate',
         'languageField' => 'sys_language_uid',
+        'transOrigPointerField' => 'l10n_parent',
         'transOrigDiffSourceField' => 'l10n_diffsource',
+        'translationSource' => 'l10n_source',
         'delete' => 'deleted',
         'enablecolumns' => [
             'disabled' => 'hidden',
